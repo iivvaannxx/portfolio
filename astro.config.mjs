1fc724fd486d9917d589d322ff1223f7fed05975
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-04-22 23:12:07 +0200

    feat: satoshi variable font

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import playformCompress from "@playform/compress";
 import qwikdev from "@qwikdev/astro";
-
-import sentry from "@sentry/astro";
-import spotlightjs from "@spotlightjs/astro";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
@@ -14,12 +11,11 @@ export default defineConfig({
   site: "https://ivanporto.io",
   srcDir: "source",
   outDir: "build",
+  output: "static",
 
   integrations: [
-    tailwind(),
+    tailwind({ applyBaseStyles: false }),
     qwikdev(),
-    sentry(),
-    spotlightjs(),
     sitemap(),
     robotsTxt(),
     playformCompress(),
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: ["./source/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Satoshi-Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
 
   plugins: [],
