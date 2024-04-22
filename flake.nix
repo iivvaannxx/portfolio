b00a50420f7578b0d23a0ecaaa99f9a7db499b09
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-04-22 20:06:54 +0200

    chore: initial commit

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: let
+
+    inherit (flake-utils.lib) eachDefaultSystem;
+    mkFlake = system: let
+
+      # The set of packages to be used.
+      pkgs = import nixpkgs { inherit system; };
+      env-name = "portfolio";
+
+      fhsEnv = pkgs.buildFHSUserEnv {
+        
+        name = "${env-name}";
+        targetPkgs = pkgs: [
+
+          pkgs.nodejs-slim
+          pkgs.nodePackages_latest.pnpm
+          
+          pkgs.bun
+          pkgs.biome
+        ];
+      };
+
+    in {
+
+      devShells.default = pkgs.mkShell {
+
+        # The packages used within the project.
+        buildInputs = [ fhsEnv ];
+        shellHook = ''
+
+          exec ${fhsEnv}/bin/${env-name}
+        '';
+      };
+    };
+
+  in eachDefaultSystem mkFlake;
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./source/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {}
+  },
+
+  plugins: []
+};
+
+export default config;
