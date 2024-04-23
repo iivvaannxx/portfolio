ac2fd43b68065779e2c7df51631deba9dd6579c4
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-04-23 20:08:52 +0200

    chore: playing with initial hero style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,31 @@ const config: Config = {
   content: ["./source/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        background: "hsl(var(--background))",
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        accent: "hsl(var(--accent))",
+        card: "hsl(var(--card))",
+        destructive: "hsl(var(--destructive))",
+        muted: "hsl(var(--muted))",
+
+        foreground: "hsl(var(--foreground))",
+        "primary-foreground": "hsl(var(--primary-foreground))",
+        "secondary-foreground": "hsl(var(--secondary-foreground))",
+        "accent-foreground": "hsl(var(--accent-foreground))",
+        "card-foreground": "hsl(var(--card-foreground))",
+        "destructive-foreground": "hsl(var(--destructive-foreground))",
+        "muted-foreground": "hsl(var(--muted-foreground))",
+
+        ring: "hsl(var(--ring))",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+
+        "gradient-start": "var(--gradient-start)",
+        "gradient-end": "var(--gradient-end)",
+      },
+
       fontFamily: {
         sans: ["Satoshi-Variable", ...defaultTheme.fontFamily.sans],
       },
