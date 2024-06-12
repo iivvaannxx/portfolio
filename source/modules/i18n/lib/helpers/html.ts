967c4284c9861b9454e152de9859d5a5645ef6dc
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-06-12 16:14:49 +0200

    feat: const assertions for html helpers

--- source/modules/i18n/lib/helpers/html.ts
+++ source/modules/i18n/lib/helpers/html.ts
@@ -1,15 +1,45 @@
-export function link(href: string, text: string, targetBlank = false) {
-  return `<a href="${href}"${targetBlank ? ' target="_blank" rel="noopener noreferrer"' : ""}>${text}</a>`;
+/**
+ * Creates an HTML link element with the specified href and text.
+ *
+ * @param href - The URL of the link.
+ * @param text - The text to be displayed as the link.
+ * @param targetBlank - Optional. Specifies whether the link should open in a new tab. Default is false.
+ * @returns The HTML link element as a string.
+ */
+export function link<const H extends string, const T extends string>(
+  href: H,
+  text: T,
+  targetBlank = false,
+) {
+  return `<a href="${href}"${targetBlank ? ' target="_blank" rel="noopener noreferrer"' : ""}>${text}</a>` as const;
 }
 
-export function strong(content: string) {
-  return `<strong>${content}</strong>`;
+/**
+ * Wraps the provided content in a <strong> HTML tag.
+ *
+ * @param content - The content to be wrapped.
+ * @returns The HTML string representing the strong element.
+ */
+export function strong<const T extends string>(content: T) {
+  return `<strong>${content}</strong>` as const;
 }
 
-export function bold(content: string) {
-  return `<b>${content}</b>`;
+/**
+ * Wraps the provided content in a <b> HTML tag.
+ *
+ * @param content - The content to be wrapped.
+ * @returns The HTML string representing the bold element.
+ */
+export function bold<const T extends string>(content: T) {
+  return `<b>${content}</b>` as const;
 }
 
-export function italic(content: string) {
-  return `<i>${content}</i>`;
+/**
+ * Wraps the provided content in a <i> HTML tag.
+ *
+ * @param content - The content to be wrapped.
+ * @returns The HTML string representing the italic element.
+ */
+export function italic<const T extends string>(content: T) {
+  return `<i>${content}</i>` as const;
 }
