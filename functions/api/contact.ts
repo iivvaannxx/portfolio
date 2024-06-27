e2f4365e526f7bf9f4adeb32745fbc85b940394d
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-06-27 20:05:28 +0200

    feat: testing emailing resend

--- functions/api/contact.ts
+++ functions/api/contact.ts
@@ -1,5 +1,9 @@
 import turnstilePlugin from "@cloudflare/pages-plugin-turnstile";
+import { Resend } from "resend";
+import { safeParse } from "valibot";
+
 import { corsHeaders } from "constants";
+import { ContactFormSchema } from "@app/modules/contact/lib/schema";
 
 /**
  * Sets the CORS headers for the preflight request.
@@ -22,9 +26,15 @@ export const onRequestOptions: PagesFunction = async (_context) => {
 const turnstileValidator: PagesFunction<Env> = async (context) => {
   return turnstilePlugin({
     secret: context.env.TURNSTILE_SECRET_KEY,
-    onError: async (...errs) => {
-      console.log("turnstileValidator onError");
-      return new Response(JSON.stringify(errs), { status: 400 });
+    onError: async (error) => {
+      const errorCodes = error.data.turnstile["error-codes"];
+      return new Response(
+        JSON.stringify({
+          message: "Error while validating the Turnstile Token",
+          data: { errorCodes },
+        }),
+        { status: 400 },
+      );
     },
   })(context);
 };
@@ -34,16 +44,31 @@ const turnstileValidator: PagesFunction<Env> = async (context) => {
  * @param context - The context object given to the function.
  */
 const sendEmail: PagesFunction<Env> = async (context) => {
-  const formData = await context.request.formData();
-  console.log("sendEmail");
+  const resend = new Resend(context.env.RESEND_API_KEY);
+  const { data, error } = await resend.emails.send({
+    from: "contact@ivanporto.io",
+    to: "dev.ivanporto@gmail.com",
 
+    subject: "[Contact Form]: New contact form submission",
+    text: "This is a test!",
+  });
+
+  /*   const formData = await context.request.formData();
+  console.log("sendEmail");
+ */
   return new Response(
-    `Successfully verified! ${JSON.stringify(context.data.turnstile)}`,
+    `Successfully verified! ${JSON.stringify({
+      data,
+      error,
+      test: safeParse(ContactFormSchema, {}),
+    })}`,
   );
 };
 
 /** The handler chain for the POST request. */
 export const onRequestPost: PagesFunction<Env>[] = [
-  turnstileValidator,
+  // Disable the turnstile validator until final deploy.
+  // It does not work in the local environment.
+  // turnstileValidator,
   sendEmail,
 ];
