dceeb093a76e09ae67216402cfc4703d21f3f493
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2024-06-29 16:46:45 +0200

    fix: more imports which caused cloudflare worker build to fail

--- source/modules/contact/lib/store.ts
+++ source/modules/contact/lib/store.ts
@@ -1,19 +1,17 @@
 import { atom, onMount } from "nanostores";
 
-import {
-  DEFAULT_LOCALE,
-  getCurrentLocale,
-  type Locale,
-} from "@app/modules/i18n";
+import { DEFAULT_LOCALE } from "@app/modules/i18n/lib/constants";
+import { getCurrentLocale } from "@app/modules/i18n/lib/utils/locales";
+import type { Locale } from "@app/modules/i18n/lib/types";
 
 /** The currently selected language. */
 export const currentLang = atom<Locale>(DEFAULT_LOCALE);
 
+// Ensure the correct language is set before is used.
 onMount(currentLang, () => {
   if (typeof window === "undefined") {
     return;
   }
 
-  // Ensure the correct language is set.
   currentLang.set(getCurrentLocale());
 });
